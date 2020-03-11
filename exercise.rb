2dd6ea4b487121fd465f8f6117dd460a08d1afdb
Yada <yadameas825@gmail.com>
2020-03-11 10:58:42 -0500

    Updated

--- exercise.rb
+++ exercise.rb
@@ -9,23 +9,47 @@
 #     puts "You are too young to drink."
 # end
 
-loop do 
-    print "Run the programm Y/N? "
-    input = gets.chomp.downcase
+# loop do 
+#     print "Run the programm Y/N? "
+#     input = gets.chomp.downcase
 
-    if input == "n"
-        break
-    elsif input == "y"
-        print "Please enter an age."
-        age = gets.chomp.to_i
+#     if input == "n"
+#         break
+#     elsif input == "y"
+#         print "Please enter an age."
+#         age = gets.chomp.to_i
 
-        if age >= 18 and age < 21
-            puts "You made it passed 18."
-        elsif age >= 21
-            puts "You are old enough to drink now."
-        else
-            puts "You are too young to drink."
-        end
-    end
-    puts
-end
+#         if age >= 18 and age < 21
+#             puts "You made it passed 18."
+#         elsif age >= 21
+#             puts "You are old enough to drink now."
+#         else
+#             puts "You are too young to drink."
+#         end
+#     end
+#     puts
+# end
+
+# Challenge 1
+first_name = "Carlo"
+last_name = "Santos"
+saving = 3000000
+checking = 2000
+
+puts "#{first_name} #{last_name} has $%.2f" % checking + " in checking and $%.2f" % saving + " in saving."
+
+#Challenge 2
+print "Please enter your first name: "
+first_name2 = gets.chomp
+
+print "Please enter your last name: "
+last_name2 = gets.chomp
+
+print "Please enter your saving balance: "
+saving2 = gets.chomp
+
+print "Please enter your checking balance: "
+checking2 = gets.chomp
+
+puts
+puts "#{first_name2.capitalize} #{last_name2.capitalize} has $%.2f" % checking2 + " in checking and $%.2f" % saving2 + " in saving."
